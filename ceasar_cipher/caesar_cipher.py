ffac36a7d250f5acc5442790c3e12f0e6faae402
Brian Lemons <brian_lemons@hotmail.com>
2021-06-10 09:29:34 -0700

    at least got the first thing done i thnk thanks google

--- ceasar_cipher/caesar_cipher.py
+++ ceasar_cipher/caesar_cipher.py
@@ -8,8 +8,17 @@ def encrypt(plain, key):
     encrypted = ""
 
     for char in plain:
-        # if char.isalpha()
-        pass
+        if (char.isalpha()):
+            if (char.isupper()):
+                encrypted += chr(((ord(char) + key - 65) % 26) + 65)
+
+            else: 
+                encrypted += chr(((ord(char) + key - 97) % 26) + 97)
+
+        else:
+            encrypted += char
+
+    return encrypted.lower()
 
 
 def decrypt(plain, key):
